730777edbe0d49ddcc99fc40fd4cb2fa88ab8b35
Imran Sadik Molla <57291277+imransadikmolla@users.noreply.github.com>
2025-01-23 12:55:53 +0530

    Create climbStairs.java

---
+++ climbStairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n==1)
+          return 1;
+        int one=1;
+        int two=2;
+        int total=two;
+        for(int i=3;i<=n;i++){
+            total=one+two;
+            one=two;
+            two=total;
+        }
+        return total;
+        
+    }
+}
